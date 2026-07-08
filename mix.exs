8ea0ed913e60fa0e553e24cb81ad020d43ddd347
Pavel Tsurbeleu <tussles.85titans@icloud.com>
2026-07-07 22:16:48 -0700

    v2.1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Socket.MixProject do
     [
       app: :sockets,
       elixir: "~> 1.12",
-      version: "2.1.3",
+      version: "2.1.4",
       deps: deps(),
       package: package(),
       description: "Socket handling library for Elixir, updated for OTP20+"
