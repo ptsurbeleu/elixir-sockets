f176094cb3ac7fd2c059a6b533e4a79aa16dd8c1
Pavel Tsurbeleu <tussles.85titans@icloud.com>
2026-07-14 18:19:16 -0700

    Socket.Address: Fix dialyzer's typespec warnings
    Socket.Address: Include unit tests for the module's public API

--- lib/socket/helpers.ex
+++ lib/socket/helpers.ex
@@ -8,6 +8,40 @@ defmodule Socket.Helpers do
     end
   end
 
+  @doc """
+  Returns true if `host` is a valid IPv4 address represented as a 4-element tuple
+  where each element is an integer in the range 0..255, e.g. `{192, 168, 1, 1}`.
+  """
+  @spec is_ipv4_address(tuple()) :: boolean()
+  defguard is_ipv4_address(host)
+           when host |> is_tuple() and tuple_size(host) == 4 and
+                  elem(host, 0) in 0..255 and
+                  elem(host, 1) in 0..255 and
+                  elem(host, 2) in 0..255 and
+                  elem(host, 3) in 0..255
+
+  @doc """
+  Returns true if `host` is a valid IPv6 address represented as an 8-element tuple
+  where each element is an integer in the range 0..65535, e.g. `{8193, 3512, 0, 0, 0, 0, 0, 1}`.
+  """
+  @spec is_ipv6_address(tuple()) :: boolean()
+  defguard is_ipv6_address(host)
+           when host |> is_tuple() and tuple_size(host) == 8 and
+                  elem(host, 0) in 0..65_535 and
+                  elem(host, 1) in 0..65_535 and
+                  elem(host, 2) in 0..65_535 and
+                  elem(host, 3) in 0..65_535 and
+                  elem(host, 4) in 0..65_535 and
+                  elem(host, 5) in 0..65_535 and
+                  elem(host, 6) in 0..65_535 and
+                  elem(host, 7) in 0..65_535
+
+  @doc """
+  Returns true if `host` is a valid IPv4 or IPv6 address tuple. See `is_ipv4_address/1`
+  and `is_ipv6_address/1` for the exact constraints on each format.
+  """
+  defguard is_ip_address(host) when is_ipv4_address(host) or is_ipv6_address(host)
+
   defmacro defbang({name, _, args}) do
     args = if is_list(args), do: args, else: []
 
