d1b8aeb81e90246b3461f170c1f8cb4ca2627538
Pavel Tsurbeleu <tussles.85titans@icloud.com>
2026-07-16 09:07:32 -0700

    Socket.Host: Minor module's refactoring and typespecs fixups
    Socket.Host: Include unit tests for the module's public API

--- lib/socket/host.ex
+++ lib/socket/host.ex
@@ -17,6 +17,8 @@ defmodule Socket.Host do
   """
   defstruct [:name, :aliases, :type, :length, :list]
 
+  use Socket.Helpers
+
   @type t :: %Socket.Host{
           name: String.t(),
           aliases: [String.t()],
@@ -24,16 +26,6 @@ defmodule Socket.Host do
           length: integer,
           list: [tuple]
         }
-
-  defp convert({_, name, aliases, type, length, list}) do
-    %Socket.Host{
-      name: List.to_string(name),
-      aliases: aliases |> Enum.map(&List.to_string/1),
-      type: type,
-      length: length,
-      list: list
-    }
-  end
 
   @doc """
   Get the hostent by address.
@@ -52,30 +44,8 @@ defmodule Socket.Host do
   @doc """
   Get the hostent by address, raising if an error occurs.
   """
-  @spec by_address!(Socket.Address.t()) :: t | no_return
-  def by_address!(address) do
-    case :inet.gethostbyaddr(Socket.Address.parse(address)) do
-      {:ok, host} ->
-        convert(host)
-
-      {:error, code} ->
-        raise Socket.Error, reason: code
-    end
-  end
-
-  @doc """
-  Get the hostent by name.
-  """
-  @spec by_name(binary) :: {:ok, t} | {:error, :inet.posix()}
-  def by_name(name) when name |> is_binary do
-    case :inet.gethostbyname(String.to_charlist(name)) do
-      {:ok, host} ->
-        {:ok, convert(host)}
-
-      {:error, reason} ->
-        {:error, reason}
-    end
-  end
+  @spec by_address!(Socket.Address.t()) :: t
+  defbang(by_address(address))
 
   @doc """
   Get the hostent by name and family.
@@ -92,63 +62,53 @@ defmodule Socket.Host do
   end
 
   @doc """
-  Get the hostent by name, raising if an error occurs.
+  Get the hostent by name.
   """
-  @spec by_name!(binary) :: t | no_return
-  def by_name!(name) when name |> is_binary do
-    case :inet.gethostbyname(String.to_charlist(name)) do
-      {:ok, host} ->
-        convert(host)
-
-      {:error, code} ->
-        raise Socket.Error, reason: code
-    end
-  end
+  @spec by_name(binary) :: {:ok, t} | {:error, :inet.posix()}
+  def by_name(name) when name |> is_binary,
+    do: by_name(name, :inet)
 
   @doc """
   Get the hostent by name and family, raising if an error occurs.
   """
-  @spec by_name!(binary | charlist, :inet.address_family()) :: t | no_return
-  def by_name!(name, family) when name |> is_binary do
-    case :inet.gethostbyname(String.to_charlist(name), family) do
-      {:ok, host} ->
-        convert(host)
+  @spec by_name!(binary, :inet.address_family()) :: t
+  defbang(by_name(name, family))
 
-      {:error, code} ->
-        raise Socket.Error, reason: code
-    end
-  end
+  @doc """
+  Get the hostent by name, raising if an error occurs.
+  """
+  @spec by_name!(binary) :: t
+  defbang(by_name(name))
 
   @doc """
   Get the hostname of the machine.
   """
   @spec name :: String.t()
   def name do
-    case :inet.gethostname() do
-      {:ok, name} ->
-        to_string(name)
-    end
+    {:ok, name} = :inet.gethostname()
+    to_string(name)
   end
 
   @doc """
   Get the interfaces of the machine.
   """
   @spec interfaces :: {:ok, [tuple]} | {:error, :inet.posix()}
-  def interfaces do
-    :inet.getifaddrs()
-  end
+  def interfaces,
+    do: :inet.getifaddrs()
 
   @doc """
   Get the interfaces of the machine, raising if an error occurs.
   """
-  @spec interfaces! :: [tuple] | no_return
-  def interfaces! do
-    case :inet.getifaddrs() do
-      {:ok, ifs} ->
-        ifs
+  @spec interfaces! :: [tuple]
+  defbang(interfaces)
 
-      {:error, code} ->
-        raise Socket.Error, reason: code
-    end
+  defp convert({_, name, aliases, type, length, list}) do
+    %Socket.Host{
+      name: List.to_string(name),
+      aliases: aliases |> Enum.map(&List.to_string/1),
+      type: type,
+      length: length,
+      list: list
+    }
   end
 end
